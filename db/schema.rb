4757dbdfe69701d3d19b18796e8ea1f5d8d4ff79
José Mota <josemota.net@gmail.com>
2013-01-26 11:11:54 +0000

    Add hstore and add meta field to images.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130125172407) do
+ActiveRecord::Schema.define(version: 20130126111112) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20130125172407) do
     t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.hstore   "meta"
   end
 
   create_table "posts", force: true do |t|
